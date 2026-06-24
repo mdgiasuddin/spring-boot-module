be9b0b5212754b8fd4ae42d7cb7050defde37e77
Md. Giash Uddin <giash.inument@bkash.com>
2026-06-24 23:22:16 +0600

    Test

--- mvc-test/src/main/java/com/example/module/mvctest/service/TestService.java
+++ mvc-test/src/main/java/com/example/module/mvctest/service/TestService.java
@@ -1,41 +1,25 @@
 package com.example.module.mvctest.service;
 
 import com.example.module.mvctest.dto.Person;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TestService {
 
-    private final Random random = new SecureRandom();
+    private final DataProviderService dataProviderService;
 
     public List<Person> getPersonList() {
-        return Arrays.asList(
-                new Person(1, "John", LocalDate.now().minusMonths(random.nextInt(100, 3000))),
-                new Person(2, "Jane", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(3, "Bob", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(4, "Alice", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(5, "Mike", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(6, "Emily", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(7, "Sarah", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(8, "David", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(9, "Emma", LocalDate.now().minusMonths(random.nextInt(100, 300))),
-                new Person(100, "Olivia", LocalDate.now().minusMonths(random.nextInt(100, 300)))
-        );
+        return dataProviderService.getPersonList();
     }
 
     public Person getPersonById(int id) {
-        return getPersonList().stream()
-                .filter(person -> person.getId() == id)
-                .findFirst()
-                .orElse(null);
+        return dataProviderService.getPersonById(id);
     }
 
 }
--- mvc-test/src/test/java/com/example/module/mvctest/service/TestServiceTest.java
+++ mvc-test/src/test/java/com/example/module/mvctest/service/TestServiceTest.java
@@ -2,35 +2,48 @@ package com.example.module.mvctest.service;
 
 import com.example.module.mvctest.dto.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class TestServiceTest {
 
-    private final TestService testService = new TestService();
+    @Mock
+    private DataProviderService dataProviderService;
+
+    @InjectMocks
+    private TestService testService;
 
     @Test
     void getPersonList_shouldReturnList() {
+        Person p1 = new Person(1, "John", LocalDate.of(1990, 1, 1));
+        Person p2 = new Person(2, "Jane", LocalDate.of(1995, 10, 20));
+        given(dataProviderService.getPersonList()).willReturn(Arrays.asList(p1, p2));
+
         List<Person> people = testService.getPersonList();
         assertNotNull(people);
-        assertEquals(10, people.size());
+        assertEquals(2, people.size());
+        assertEquals("John", people.getFirst().getName());
     }
 
     @Test
     void getPersonById_shouldReturnPersonForValidId() {
+        Person p1 = new Person(1, "John", LocalDate.of(1990, 1, 1));
+        given(dataProviderService.getPersonById(anyInt())).willReturn(p1);
+
         Person person = testService.getPersonById(1);
         assertNotNull(person);
-        assertEquals(1, person.getId());
         assertEquals("John", person.getName());
-    }
-
-    @Test
-    void getPersonById_shouldReturnNullForInvalidId() {
-        Person person = testService.getPersonById(999);
-        assertNull(person);
     }
 }
