7e452de9b1d8665e68c796732589b214b99ff990
Md. Giash Uddin <giash.inument@bkash.com>
2026-07-02 22:26:16 +0600

    Use sequence for id generator

--- spring-batch/src/main/java/com/example/module/springbatch/batch/BatchConfig.java
+++ spring-batch/src/main/java/com/example/module/springbatch/batch/BatchConfig.java
@@ -3,24 +3,30 @@ package com.example.module.springbatch.batch;
 
 import com.example.module.springbatch.dto.EmployeeCsvDto;
 import com.example.module.springbatch.entity.Employee;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.database.JpaItemWriter;
+import org.springframework.batch.infrastructure.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.infrastructure.item.file.FlatFileItemReader;
 import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.infrastructure.item.support.SynchronizedItemStreamReader;
+import org.springframework.batch.infrastructure.item.support.builder.SynchronizedItemStreamReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
 
     @Bean
-    public FlatFileItemReader<EmployeeCsvDto> reader() {
-        return new FlatFileItemReaderBuilder<EmployeeCsvDto>()
+    public SynchronizedItemStreamReader<EmployeeCsvDto> reader() {
+        FlatFileItemReader<EmployeeCsvDto> reader = new FlatFileItemReaderBuilder<EmployeeCsvDto>()
                 .name("employeeFileReader")
                 .resource(new ClassPathResource("csv/employees.csv"))
                 .linesToSkip(1)
@@ -28,26 +34,48 @@ public class BatchConfig {
                 .names("name", "department", "email", "salary", "dob")
                 .targetType(EmployeeCsvDto.class)
                 .build();
+        return new SynchronizedItemStreamReaderBuilder<EmployeeCsvDto>()
+                .delegate(reader)
+                .build();
+    }
+
+    @Bean
+    public JpaItemWriter<Employee> writer(EntityManagerFactory entityManagerFactory) {
+        return new JpaItemWriterBuilder<Employee>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
     }
 
     @Bean
     public Step employeeStep(
             JobRepository jobRepository,
             PlatformTransactionManager transactionManager,
-            FlatFileItemReader<EmployeeCsvDto> reader,
+            SynchronizedItemStreamReader<EmployeeCsvDto> reader,
             EmployeeItemProcessor processor,
-            EmployeeItemWriter writer
+            JpaItemWriter<Employee> writer,
+            ThreadPoolTaskExecutor taskExecutor
     ) {
         return new StepBuilder("insertEmployeeStep", jobRepository)
-                .<EmployeeCsvDto, Employee>chunk(100)
+                .<EmployeeCsvDto, Employee>chunk(50)
                 .transactionManager(transactionManager)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
+                .taskExecutor(taskExecutor)
                 .build();
     }
 
-    //    @Bean
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(25);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Bean
     public Job insertEmployeeJob(JobRepository jobRepository, Step subscriptionStep) {
         return new JobBuilder("insertEmployeeJob", jobRepository)
                 .start(subscriptionStep)
--- spring-batch/src/main/java/com/example/module/springbatch/entity/Employee.java
+++ spring-batch/src/main/java/com/example/module/springbatch/entity/Employee.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static jakarta.persistence.GenerationType.SEQUENCE;
 
 @Entity
 @Table
@@ -23,7 +24,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(strategy = SEQUENCE, generator = "employee_seq")
+    @SequenceGenerator(name = "employee_seq", sequenceName = "employee_id_seq")
     private Long id;
 
     @Column(nullable = false)
