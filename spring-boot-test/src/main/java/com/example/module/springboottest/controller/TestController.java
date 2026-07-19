9c25bea9b2b85fe9ecbff60d924547a02796ca0c
Md. Giash Uddin <giash.inument@bkash.com>
2026-07-19 14:13:51 +0600

    Add number to word

--- spring-boot-test/src/main/java/com/example/module/springboottest/controller/TestController.java
+++ spring-boot-test/src/main/java/com/example/module/springboottest/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.example.module.springboottest.controller;
 
 import com.example.module.springboottest.dto.Person;
+import com.example.module.springboottest.util.NumberWordConverter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final NumberWordConverter numberWordConverter;
+
     @GetMapping
     public String test() {
         log.info("test");
@@ -25,5 +32,10 @@ public class TestController {
                 "01711111111",
                 "Dhaka, Bangladesh"
         );
+    }
+
+    @GetMapping("/number-to-words")
+    public String getNumber(@RequestParam long number) {
+        return numberWordConverter.convertToWords(number);
     }
 }
---
+++ spring-boot-test/src/main/java/com/example/module/springboottest/util/NumberWordConverter.java
@@ -0,0 +1,68 @@
+package com.example.module.springboottest.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class NumberWordConverter {
+
+    private final String[] units = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String convertToWords(long number) {
+        if (number == 0) {
+            return "Zero";
+        }
+
+        List<String> words = new ArrayList<>();
+        int crorePart = Math.toIntExact(number / 10000000);
+        int otherPart = Math.toIntExact(number % 10000000);
+
+        if (crorePart > 0) {
+            convertToWords(crorePart, words);
+            words.add("Crore");
+        }
+        convertToWords(otherPart, words);
+
+        return String.join(" ", words);
+    }
+
+    private void convertToWords(int number, List<String> words) {
+        int lakh = Math.toIntExact((number % 10000000) / 100000);
+        int thousand = Math.toIntExact((number % 100000) / 1000);
+        int hundred = Math.toIntExact((number % 1000) / 100);
+        int tenOne = Math.toIntExact(number % 100);
+
+        convertToWords(lakh, "Lakh", words);
+        convertToWords(thousand, "Thousand", words);
+        convertToWords(hundred, "Hundred", words);
+        convertToWords(tenOne, "", words);
+    }
+
+    private void convertToWords(int number, String unit, List<String> words) {
+        if (number == 0) {
+            return;
+        }
+
+        convert(number, words);
+        if (!unit.isEmpty()) words.add(unit);
+    }
+
+    private void convert(int number, List<String> words) {
+        if (number < 20) {
+            words.add(units[number]);
+            return;
+        }
+
+        int tensPlace = number / 10;
+        int onesPlace = number % 10;
+        words.add(tens[tensPlace]);
+        if (onesPlace != 0) {
+            words.add(units[onesPlace]);
+        }
+    }
+}
