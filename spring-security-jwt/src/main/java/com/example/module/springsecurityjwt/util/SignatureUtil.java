3363df70439df1191ec6e701989940accaccec93
Md. Giash Uddin <giash.inument@bkash.com>
2026-07-21 15:21:13 +0600

    Fixed field name standard.

--- spring-security-jwt/src/main/java/com/example/module/springsecurityjwt/util/SignatureUtil.java
+++ spring-security-jwt/src/main/java/com/example/module/springsecurityjwt/util/SignatureUtil.java
@@ -4,7 +4,9 @@ import lombok.NoArgsConstructor;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -13,21 +15,21 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access = PRIVATE)
 public class SignatureUtil {
 
-    private static final Base64.Encoder base64UrlEncoder = Base64.getUrlEncoder();
-    private static final Base64.Decoder base64UrlDecoder = Base64.getUrlDecoder();
-    private static final String algorithm = "HmacSHA256";
+    private static final Base64.Encoder BASE64_URL_ENCODER = Base64.getUrlEncoder().withoutPadding();
+    private static final Base64.Decoder BASE_URL_DECODER = Base64.getUrlDecoder();
+    private static final String ALGORITHM = "HmacSHA256";
 
     public static String sign(String securityKey, String payload) {
-        byte[] decodedKey = base64UrlDecoder.decode(securityKey);
-        SecretKeySpec keySpec = new SecretKeySpec(decodedKey, algorithm);
+        byte[] decodedKey = BASE_URL_DECODER.decode(securityKey);
+        SecretKeySpec keySpec = new SecretKeySpec(decodedKey, ALGORITHM);
 
         try {
-            Mac mac = Mac.getInstance(algorithm);
+            Mac mac = Mac.getInstance(ALGORITHM);
             mac.init(keySpec);
             byte[] bytes = mac.doFinal(payload.getBytes(UTF_8));
 
-            return base64UrlEncoder.encodeToString(bytes);
-        } catch (Exception e) {
+            return BASE64_URL_ENCODER.encodeToString(bytes);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
